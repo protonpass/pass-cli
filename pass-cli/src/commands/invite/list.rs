bcee3184343f653ee700cff61b17cdc5b7bd9b26
Carlos Quintana <carlos.quintana@proton.ch>
2025-09-05 15:49:43 +0200

    feat: offer invite list as JSON

--- pass-cli/src/commands/invite/list.rs
+++ pass-cli/src/commands/invite/list.rs
@@ -3,6 +3,34 @@ use anyhow::{Context, Result};
 use pass::PassClient;
 use pass_domain::Invite;
 
+#[derive(serde::Serialize)]
+struct InviteList {
+    invites: Vec<InviteEntry>,
+}
+
+#[derive(serde::Serialize)]
+struct InviteEntry {
+    invite_type: String,
+    inviter: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+}
+
+impl InviteEntry {
+    pub fn from(invite: Invite) -> Self {
+        let (invite_type, name) = match invite.vault_data {
+            Some(data) => ("vault".to_string(), Some(data.vault_data.name)),
+            None => ("item".to_string(), None),
+        };
+
+        Self {
+            invite_type,
+            inviter: invite.inviter_email,
+            name,
+        }
+    }
+}
+
 pub async fn run(client: PassClient, output: OutputFormat) -> Result<()> {
     let invites = client
         .list_user_invites()
@@ -31,7 +59,12 @@ pub async fn run(client: PassClient, output: OutputFormat) -> Result<()> {
             }
         }
         OutputFormat::Json => {
-            todo!()
+            let mapped: Vec<InviteEntry> = invites.into_iter().map(InviteEntry::from).collect();
+            let instance = InviteList { invites: mapped };
+            let as_json =
+                serde_json::to_string_pretty(&instance).context("Error serializing invite list")?;
+
+            println!("{}", as_json);
         }
     }
 
