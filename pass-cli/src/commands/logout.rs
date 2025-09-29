fc43a999f5a83514fe494fdb235f4da2dd55d1dd
Carlos Quintana <carlos.quintana@proton.ch>
2025-09-29 10:37:12 +0200

    chore: unify local data removal into single function

--- pass-cli/src/commands/logout.rs
+++ pass-cli/src/commands/logout.rs
@@ -2,27 +2,7 @@ use crate::utils::get_base_dir;
 use anyhow::{Context, Result, anyhow};
 use pass::PassClient;
 
-pub async fn run(client: PassClient) -> Result<()> {
-    client.logout().await.context("Error logging out")?;
-
-    let base_dir = get_base_dir().context("Error getting base dir")?;
-    if base_dir.is_dir() {
-        tokio::fs::remove_dir_all(&base_dir)
-            .await
-            .context("Error deleting base dir")?;
-    } else {
-        return Err(anyhow!(
-            "Base directory is not a directory: {}",
-            base_dir.display()
-        ));
-    }
-
-    println!("Successfully logged out");
-    Ok(())
-}
-
-pub async fn force_logout() -> Result<()> {
-    println!("Executing force logout");
+async fn remove_local_data() -> Result<()> {
     let base_dir = get_base_dir().context("Error getting base dir")?;
     if !base_dir.exists() {
         println!("There was no data to be removed");
@@ -33,8 +13,6 @@ pub async fn force_logout() -> Result<()> {
         tokio::fs::remove_dir_all(&base_dir)
             .await
             .context("Error deleting base dir")?;
-
-        println!("Successfully performed force logout");
         Ok(())
     } else {
         Err(anyhow!(
@@ -42,4 +20,19 @@ pub async fn force_logout() -> Result<()> {
             base_dir.display()
         ))
     }
+}
+
+pub async fn run(client: PassClient) -> Result<()> {
+    client.logout().await.context("Error logging out")?;
+
+    remove_local_data().await?;
+    println!("Successfully logged out");
+    Ok(())
+}
+
+pub async fn force_logout() -> Result<()> {
+    println!("Executing force logout");
+    remove_local_data().await?;
+    println!("Successfully performed force logout");
+    Ok(())
 }
