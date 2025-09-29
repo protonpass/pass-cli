0f53e146c38ab016766427a6a80b2409bc4bd832
Carlos Quintana <carlos.quintana@proton.ch>
2025-09-29 09:08:40 +0200

    refactor: keyring provider

--- pass-cli/src/features/keyring.rs
+++ pass-cli/src/features/keyring.rs
@@ -1,8 +1,7 @@
-use anyhow::Context;
-use keyring::{Entry, Error};
+use anyhow::{Context, Result};
+use keyring::{Entry, Error as KeyringError};
 use pass_domain::LocalKeyProvider;
 use tokio::sync::RwLock;
-use tracing::info;
 
 const KEYRING_SERVICE_NAME: &str = "ProtonPassCLI";
 const KEYRING_CREDENTIAL_NAME: &str = "cli-local-key";
@@ -10,6 +9,37 @@ const KEYRING_CREDENTIAL_NAME: &str = "cli-local-key";
 #[derive(Default)]
 pub struct KeyringKeyProvider {
     key: RwLock<Option<Vec<u8>>>,
+}
+
+impl KeyringKeyProvider {
+    fn build_entry() -> Result<Entry> {
+        let entry = Entry::new(KEYRING_SERVICE_NAME, KEYRING_CREDENTIAL_NAME)
+            .map_err(|e| anyhow::anyhow!("Error accessing credential: {e:?}"))?;
+
+        Ok(entry)
+    }
+
+    async fn get_local_key() -> Result<Vec<u8>> {
+        let entry = Self::build_entry()?;
+
+        match entry.get_secret() {
+            Ok(cred) => Ok(cred),
+            Err(e) => match e {
+                KeyringError::NoEntry => {
+                    info!("Credential not found in Keyring. Creating one");
+                    let cred = pass_domain::crypto::generate_encryption_key();
+                    entry
+                        .set_secret(&cred)
+                        .map_err(|e| anyhow::anyhow!("Error accessing keyring: {e}"))?;
+                    info!("Stored credential into keyring");
+                    Ok(cred)
+                }
+                _ => Err(anyhow::anyhow!(
+                    "Error accessing credential on keyring: {e:?}"
+                )),
+            },
+        }
+    }
 }
 
 #[async_trait::async_trait]
@@ -21,34 +51,11 @@ impl LocalKeyProvider for KeyringKeyProvider {
         } else {
             drop(key_guard);
             let mut write_key_guard = self.key.write().await;
-            let key = get_local_key()
+            let key = Self::get_local_key()
                 .await
                 .context("Could not get local key from keyring")?;
             *write_key_guard = Some(key.clone());
             Ok(key)
         }
-    }
-}
-
-pub async fn get_local_key() -> anyhow::Result<Vec<u8>> {
-    let entry = Entry::new(KEYRING_SERVICE_NAME, KEYRING_CREDENTIAL_NAME)
-        .map_err(|e| anyhow::anyhow!("Error accessing credential: {e:?}"))?;
-
-    match entry.get_secret() {
-        Ok(cred) => Ok(cred),
-        Err(e) => match e {
-            Error::NoEntry => {
-                info!("Credential not found in Keyring. Creating one");
-                let cred = pass_domain::crypto::generate_encryption_key();
-                entry
-                    .set_secret(&cred)
-                    .map_err(|e| anyhow::anyhow!("Error accessing keyring: {e}"))?;
-                info!("Stored credential into keyring");
-                Ok(cred)
-            }
-            _ => Err(anyhow::anyhow!(
-                "Error accessing credential on keyring: {e:?}"
-            )),
-        },
     }
 }
